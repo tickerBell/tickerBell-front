988645b276324fc305c08c46590c2daefe3704a5
rudwnok <rudwnok123@naver.com>
2023-10-12 22:03:50 +0900

    메뉴에 isdev 임시 제외 - 배포 환경 테스트

--- src/components/portalModal/mapModal/MapModal.tsx
+++ src/components/portalModal/mapModal/MapModal.tsx
@@ -23,75 +23,75 @@ const MapModal = ({ setOnModal, dimClick, isDim = true, className }: MapModalTyp
   console.log('user 스토어', getLocation);
 
   // NOTE: 임시조치 - next.config.js의 배포와 충돌
-  // useEffect(() => {
-  //   // 주소로 위도 경도 찾기 - 목적지
-  //   axios
-  //     .get("http://localhost:3000/api/navermap", {
-  //       params: {
-  //         query: "서울 관악구 관악로 1",
-  //       },
-  //     })
-  //     // 경로 탐색
-  //     .then((response) => {
-  //       console.log('변환 : ', response);
-  //       console.log('컨버터1: ', response.data.data.addresses[0].x)
-  //       console.log('컨버터2: ', response.data.data.addresses[0].y)
-  //       axios
-  //         .get("http://localhost:3000/api/path", {
-  //           params: {
-  //             start: `${getLocation.longitude},${getLocation.latitude}`,
-  //             goal: `${response.data.data.addresses[0].x}, ${response.data.data.addresses[0].y}`,
-  //             option: "trafast"
-  //           },
-  //         })
-  //         .then((response) => {
-  //           console.log('경로탐색 : ', response.data.data.route.trafast[0].path);
-  //           // 경로 정보 설정
-  //           setRoute(response.data.data.route.trafast[0].path);
-  //         })
-  //     })
+  useEffect(() => {
+    // 주소로 위도 경도 찾기 - 목적지
+    axios
+      .get("http://localhost:3000/api/navermap", {
+        params: {
+          query: "서울 관악구 관악로 1",
+        },
+      })
+      // 경로 탐색
+      .then((response) => {
+        console.log('변환 : ', response);
+        console.log('컨버터1: ', response.data.data.addresses[0].x)
+        console.log('컨버터2: ', response.data.data.addresses[0].y)
+        axios
+          .get("http://localhost:3000/api/path", {
+            params: {
+              start: `${getLocation.longitude},${getLocation.latitude}`,
+              goal: `${response.data.data.addresses[0].x}, ${response.data.data.addresses[0].y}`,
+              option: "trafast"
+            },
+          })
+          .then((response) => {
+            console.log('경로탐색 : ', response.data.data.route.trafast[0].path);
+            // 경로 정보 설정
+            setRoute(response.data.data.route.trafast[0].path);
+          })
+      })
 
-  // }, [getLocation])
+  }, [getLocation])
 
-  // useEffect(() => {
-  //   if (route.length > 0) {
-  //     const map = new window.naver.maps.Map('map', {
-  //       center: new window.naver.maps.LatLng(getLocation.latitude, getLocation.longitude), // 초기 지도 중심 좌표 설정
-  //       zoom: 14, // 초기 줌 레벨 설정 - 높을수록 확대
-  //     });
+  useEffect(() => {
+    if (route.length > 0) {
+      const map = new window.naver.maps.Map('map', {
+        center: new window.naver.maps.LatLng(getLocation.latitude, getLocation.longitude), // 초기 지도 중심 좌표 설정
+        zoom: 14, // 초기 줌 레벨 설정 - 높을수록 확대
+      });
 
-  //     // 경로를 표시할 Polyline 추가
-  //     const polyline = new window.naver.maps.Polyline({
-  //       map,
-  //       path: route,
-  //       strokeColor: '#ff0000', // 선 색상
-  //       strokeWeight: 3, // 선 굵기
-  //     });
+      // 경로를 표시할 Polyline 추가
+      const polyline = new window.naver.maps.Polyline({
+        map,
+        path: route,
+        strokeColor: '#ff0000', // 선 색상
+        strokeWeight: 3, // 선 굵기
+      });
 
-  //     const path = route.map(coordPair => {
-  //       const [latitude, longitude] = coordPair;
-  //       return new window.naver.maps.LatLng(longitude, latitude);
-  //     });
+      const path = route.map(coordPair => {
+        const [latitude, longitude] = coordPair;
+        return new window.naver.maps.LatLng(longitude, latitude);
+      });
 
 
-  //     // 출발지, 경유지, 도착지 마커 추가
-  //     const markers = route.map((coordinate, index) => {
-  //       if (index === 0 || index === path.length - 1) {
-  //         return new window.naver.maps.Marker({
-  //           position: coordinate,
-  //           map
-  //         });
-  //       }
-  //       return null;
-  //     }).filter(marker => marker !== null);
+      // 출발지, 경유지, 도착지 마커 추가
+      const markers = route.map((coordinate, index) => {
+        if (index === 0 || index === path.length - 1) {
+          return new window.naver.maps.Marker({
+            position: coordinate,
+            map
+          });
+        }
+        return null;
+      }).filter(marker => marker !== null);
 
-  //     return () => {
-  //       // 컴포넌트가 언마운트되면 지도 객체를 정리
-  //       markers.forEach((marker: any) => marker.setMap(null));
-  //       polyline.setMap(null);
-  //     };
-  //   }
-  // }, [route, getLocation])
+      return () => {
+        // 컴포넌트가 언마운트되면 지도 객체를 정리
+        markers.forEach((marker: any) => marker.setMap(null));
+        polyline.setMap(null);
+      };
+    }
+  }, [route, getLocation])
 
   return (
     <ModalFrame
