8b84770c92a7fae52572167ab99e25478f54282c
rudwnok <rudwnok123@naver.com>
2023-10-14 20:54:27 +0900

    serachparams 주석

--- src/app/api/navermap/route.tsx
+++ src/app/api/navermap/route.tsx
@@ -6,25 +6,25 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { type NextRequest, NextResponse } from "next/server";
 import { useSearchParams } from "next/navigation";
 
-export async function GET(req: NextRequest, res: NextApiResponse) {
-  // console.log(name, keyword, method);
+// export async function GET(req: NextRequest, res: NextApiResponse) {
+//   // console.log(name, keyword, method);
 
-  const searchParams = req.nextUrl.searchParams;
-  console.log("dd", searchParams);
-  const query = searchParams.get("query");
-  const response = await axios.get("https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode", {
-    params: {
-      query: query,
-    },
-    headers: {
-      "X-NCP-APIGW-API-KEY-ID": "ugm16gkcw2",
-      "X-NCP-APIGW-API-KEY": "mDTqyhP8j8bw0IPW8dOaPDG0Wm2VRiSzyqvMotRw",
-    },
-  });
-  const data = await response.data;
-  return NextResponse.json({ data });
-  // return res.status(200).json({ data });
-}
+//   const searchParams = req.nextUrl.searchParams;
+//   console.log("dd", searchParams);
+//   const query = searchParams.get("query");
+//   const response = await axios.get("https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode", {
+//     params: {
+//       query: query,
+//     },
+//     headers: {
+//       "X-NCP-APIGW-API-KEY-ID": "ugm16gkcw2",
+//       "X-NCP-APIGW-API-KEY": "mDTqyhP8j8bw0IPW8dOaPDG0Wm2VRiSzyqvMotRw",
+//     },
+//   });
+//   const data = await response.data;
+//   return NextResponse.json({ data });
+//   // return res.status(200).json({ data });
+// }
 
 // export async function GET(request: NextRequest) {
 //   const searchParams = request.nextUrl.searchParams
@@ -33,8 +33,8 @@ export async function GET(req: NextRequest, res: NextApiResponse) {
 // }
 
 
-// export async function GET(request: NextRequest) {
-//   const response = await axios.get("https://pokeapi.co/api/v2/pokemon/1", {})
-//   const data = await response.data;
-//   return NextResponse.json({ data })
-// }
+export async function GET(request: NextRequest) {
+  const response = await axios.get("https://pokeapi.co/api/v2/pokemon/1", {})
+  const data = await response.data;
+  return NextResponse.json({ data })
+}
