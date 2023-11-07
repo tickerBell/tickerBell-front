2dbbf2837bdb675aa4cdd7661c81057d4c9d377b
rudwnok <rudwnok123@naver.com>
2023-11-07 19:50:38 +0900

    정적 데이터 분리

---
+++ src/components/HistoryTable/TableData.ts
@@ -0,0 +1,20 @@
+export const UserColumns: IUserColumnsData[] = [
+  {
+    name: "이벤트명",
+    speaker: "캐스팅",
+    date: "일시",
+    place: "장소",
+  },
+];
+
+export const EventColumns: IEventColumnsData[] = [
+  {
+    name: "이벤트명",
+    speaker: "캐스팅",
+    date: "일시",
+    place: "장소",
+    totaluser: "예매좌수",
+    maxseats: "최대좌석",
+    cancel: "취소",
+  },
+];
