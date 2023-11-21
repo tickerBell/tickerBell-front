e79dfbafe12196c1481e0ff4943baf77746401cc
rudwnok <rudwnok123@naver.com>
2023-11-21 23:49:46 +0900

    애니 타입 추가

--- src/hooks/useParseJwt.ts
+++ src/hooks/useParseJwt.ts
@@ -1,4 +1,4 @@
-export function parseJwt(token) {
+export function parseJwt(token:any) {
   if(token) {
     var base64Url = token.split('.')[1];
     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
